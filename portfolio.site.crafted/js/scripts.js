861c7b2d0ce849eaa7b5b3254014c4ba3b485230
Dave Sayer <hello@crafted.im>
2018-10-03 23:09:47 +0100

    reworking design and playing with some js.

---
+++ portfolio.site.crafted/js/scripts.js
@@ -0,0 +1,11 @@
+// Can also be included with a regular script tag
+
+/* typed effect */
+var options = {
+    strings: ["Web devl", "Web development", "Web design", "Ecommerce ^1000", "Hand crafted websites made in the Isle of Man"],
+    typeSpeed: 100,
+    backDelay: 200,
+    loop: false,
+    loopCount: Infinity
+}
+var typed = new Typed('#typed', options);
